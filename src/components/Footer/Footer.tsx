3f09a9cca87662ab5c0c103b78189ce0c61a73ff
JorgeD13 <jorge.castillo@utec.edu.pe>
2025-11-25 17:55:55 -0500

    fix: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -25,112 +25,119 @@ export const Footer = ({
   ...props
 }: FooterProps) => {
   return (
-    <footer {...props} tabIndex={0} aria-label="Información para contactarnos">
+    <footer {...props} tabIndex={0} aria-label='Información para contactarnos'>
       {showFooterContent && (
         <>
           {children && children}
 
           {showBrowserInfo && <BrowserRecommended />}
           {showContactInfo && (
-            <div className="onpe-footer-contact">
-              <ul className="onpe-footer-contact-list">
-                <li tabIndex={0} className="onpe-footer-contact-item">
-                  <p className="onpe-footer-contact-title">Oficina central</p>
-                  <p className="onpe-footer-contact-text">
-                    <span className="sr-only">Jiron</span>
-                    <span aria-hidden="true">Jr.</span> Washington 1894 -
-                    Cercado de Lima
+            <div className='onpe-footer-contact'>
+              <ul className='onpe-footer-contact-list'>
+                <li tabIndex={0} className='onpe-footer-contact-item'>
+                  <p className='onpe-footer-contact-title'>Oficina central</p>
+                  <p className='onpe-footer-contact-text'>
+                    <span className='sr-only'>
+                      Dirección: Jiron Washington 1894 - Cercado de Lima
+                    </span>
+                    <span aria-hidden='true'>Jr. Washington 1894 - Cercado de Lima</span>
                   </p>
-                  <p className="onpe-footer-contact-text">
-                    Lunes a viernes de 8:30 a.m. a 5:00 p. m.
+                  <p className='onpe-footer-contact-text'>
+                    <span className='sr-only'>
+                      Horario de atención: Lunes a viernes de 8:30 a.m. a 5:00
+                      p. m.
+                    </span>
+                    <span aria-hidden='true'>
+                      Lunes a viernes de 8:30 a.m. a 5:00 p. m.
+                    </span>
                   </p>
                 </li>
-                <li tabIndex={0} className="onpe-footer-contact-item">
-                  <p className="onpe-footer-contact-title">Contáctanos:</p>
+                <li tabIndex={0} className='onpe-footer-contact-item'>
+                  <p className='onpe-footer-contact-title'>Contáctanos:</p>
                   <p
-                    className="onpe-footer-contact-text"
-                    aria-label="Correo electrónico, informes@onpe punto goob punto pe,"
+                    className='onpe-footer-contact-text'
+                    aria-label='Correo electrónico, informes@onpe punto goob punto pe,'
                   >
                     informes@onpe.gob.pe
                   </p>
                   <p
-                    className="onpe-footer-contact-text"
-                    aria-label="Número de Teléfono, +51 (01)4170630,"
+                    className='onpe-footer-contact-text'
+                    aria-label='Número de Teléfono, +51 (01)4170630,'
                   >
                     +51 (01)4170630
                   </p>
-                  <p className="onpe-footer-contact-text">
+                  <p className='onpe-footer-contact-text'>
                     <a
-                      href="https://wa.me/+51995404991"
-                      className="flex"
-                      target="_blank"
-                      aria-hidden="true"
+                      href='https://wa.me/+51995404991'
+                      className='flex'
+                      target='_blank'
+                      aria-label='Whatsapp: 995 404 991'
                     >
-                      <WhatsappIcon aria-hidden="true" />
+                      <WhatsappIcon aria-hidden='true' />
                       &nbsp; Whatsapp: 995 404 991
                     </a>
                   </p>
                 </li>
-                <li tabIndex={0} className="onpe-footer-contact-item">
+                <li tabIndex={0} className='onpe-footer-contact-item'>
                   <p
-                    className="onpe-footer-contact-title"
-                    aria-label="Síguenos en nuestra redes sociales"
+                    className='onpe-footer-contact-title'
+                    aria-label='Síguenos en nuestra redes sociales'
                   >
                     Síguenos:
                   </p>
-                  <p className="onpe-footer-contact-text flex gap-4">
+                  <p className='onpe-footer-contact-text flex gap-4'>
                     <a
-                      href="https://www.facebook.com/ONPEoficial"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="Facebook de ONPE,"
-                      className="onpe-footer-contact-item-color"
+                      href='https://www.facebook.com/ONPEoficial'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='Facebook de ONPE,'
+                      className='onpe-footer-contact-item-color'
                     >
-                      <span aria-hidden="true">
+                      <span aria-hidden='true'>
                         <FaceBookIcon />
                       </span>
                     </a>
                     <a
-                      href="https://x.com/ONPE_oficial"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="X de ONPE (antes Twitter),"
-                      className="onpe-footer-contact-item-color"
+                      href='https://x.com/ONPE_oficial'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='X de ONPE (antes Twitter),'
+                      className='onpe-footer-contact-item-color'
                     >
-                      <span aria-hidden="true">
+                      <span aria-hidden='true'>
                         <XIcon />
                       </span>
                     </a>
                     <a
-                      href="https://www.tiktok.com/@onpe_oficial"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="TikTok de ONPE,"
-                      className="onpe-footer-contact-item-color"
+                      href='https://www.tiktok.com/@onpe_oficial'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='TikTok de ONPE,'
+                      className='onpe-footer-contact-item-color'
                     >
-                      <span aria-hidden="true">
+                      <span aria-hidden='true'>
                         <TikTokIcon />
                       </span>
                     </a>
                     <a
-                      href="https://www.instagram.com/ONPE_oficial/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="Instagram de ONPE,"
-                      className="onpe-footer-contact-item-color"
+                      href='https://www.instagram.com/ONPE_oficial/'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='Instagram de ONPE,'
+                      className='onpe-footer-contact-item-color'
                     >
-                      <span aria-hidden="true">
+                      <span aria-hidden='true'>
                         <InstagramIcon />
                       </span>
                     </a>
                     <a
-                      href="https://www.youtube.com/@onpeprensa"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="YouTube de ONPE,"
-                      className="onpe-footer-contact-item-color"
+                      href='https://www.youtube.com/@onpeprensa'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='YouTube de ONPE,'
+                      className='onpe-footer-contact-item-color'
                     >
-                      <span aria-hidden="true">
+                      <span aria-hidden='true'>
                         <YoutubeIcon />
                       </span>
                     </a>
@@ -143,10 +150,10 @@ export const Footer = ({
       )}
 
       {isDevelopment && (
-        <div className="onpe-footer-development">
-          <p className="onpe-footer-development-text">
-            Versión en Desarrollo{" "}
-            <br className="onpe-footer-development-break" /> - No Oficial
+        <div className='onpe-footer-development'>
+          <p className='onpe-footer-development-text'>
+            Versión en Desarrollo{' '}
+            <br className='onpe-footer-development-break' /> - No Oficial
           </p>
         </div>
       )}
