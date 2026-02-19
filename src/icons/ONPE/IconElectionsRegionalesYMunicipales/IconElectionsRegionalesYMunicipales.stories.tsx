b50f288cf76c9adbcb14ef4a7087951f288f5499
Jorge <jcastilloa@onpe.gob.pe>
2026-02-19 12:04:43 -0500

    1.3.43

---
+++ src/icons/ONPE/IconElectionsRegionalesYMunicipales/IconElectionsRegionalesYMunicipales.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { IconElectionsRegionalesYMunicipales } from "./IconElectionsRegionalesYMunicipales";
+
+// @ts-ignore
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof IconElectionsRegionalesYMunicipales> = {
+  title: "Icons/Logos/IconElectionsRegionalesYMunicipales",
+  component: IconElectionsRegionalesYMunicipales,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    width: {
+      control: { type: "number" },
+    },
+    height: {
+      control: { type: "number" },
+    },
+    className: {
+      control: { type: "text" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    width: 230,
+    height: 80,
+  },
+};
+
+export const Small: Story = {
+  args: {
+    width: 115,
+    height: 40,
+  },
+};
+
+export const Large: Story = {
+  args: {
+    width: 460,
+    height: 160,
+  },
+};
+
+export const CustomStyling: Story = {
+  args: {
+    width: 230,
+    height: 80,
+    className: "border-2 border-blue-500 rounded-lg p-4",
+  },
+};
