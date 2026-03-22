bcf8c522e8a2730d1726da42e03f00b4dff2ea9b
Ricardo <rsolis@onpe.gob.pe>
2026-03-22 13:55:32 -0500

    feat: Introduce a new `Modal` component with global context, focus management, and body scroll locking capabilities.

--- src/components/ModalGlobal/ModalGlobalContext.ts
+++ src/components/ModalGlobal/ModalGlobalContext.ts
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 
 interface ModalGlobalContextValue {
   animated: boolean;
+  disableFocus: boolean;
 }
 
 export const ModalGlobalContext = createContext<ModalGlobalContextValue | null>(null);
